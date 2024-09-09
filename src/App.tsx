284949a5b09749fc2810e1f34378f40ecccde760
Jim Segal <hello@jimsegal.com>
2024-09-09 11:37:41 -0500

    delete app.tsx

--- src/App.tsx
+++
@@ -1,5 +0,0 @@
-function App() {
-  return <h1>JimSegal.com</h1>;
-}
-
-export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,5 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import App from "./App.tsx";
-
 import { RouterProvider, createRouter } from "@tanstack/react-router";
 
 import { routeTree } from "./routeTree.gen";
