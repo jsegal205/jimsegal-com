d5a2101b5dd24a6ee8c6777d8d36de74b25c7535
Jim Segal <hello@jimsegal.com>
2024-09-13 13:02:59 -0500

    add search to recipes

--- src/routes/_withnav/recipes/index.tsx
+++ src/routes/_withnav/recipes/index.tsx
@@ -1,15 +1,34 @@
+import { useEffect, useState } from "react";
 import { createFileRoute, Link } from "@tanstack/react-router";
 
 import { fetchAllRecipes } from "@/api/recipes";
-import { type Recipe } from "@/api/recipes";
+import { type Recipe, type Recipes } from "@/api/recipes";
 import { Loading } from "@/components/loading";
+import { Search, SearchEmptyResults } from "@/components/search";
 
 export const Route = createFileRoute("/_withnav/recipes/")({
   component: () => <Recipes />,
 });
 
 const Recipes = () => {
+  const [recipes, setRecipes] = useState<Recipes>([]);
   const { isError, isPending, data, error } = fetchAllRecipes();
+
+  useEffect(() => {
+    setRecipes(data || []);
+  }, [data]);
+
+  const searchCallback = (search: string) => {
+    if (search) {
+      const filteredRecipes = data!.filter((recipe: Recipe) =>
+        recipe.title.toLowerCase().includes(search.toLowerCase()),
+      );
+
+      setRecipes(filteredRecipes);
+    } else {
+      setRecipes(data!);
+    }
+  };
 
   if (isPending) {
     return <Loading />;
@@ -22,8 +41,9 @@ const Recipes = () => {
   return (
     <>
       <h1>Recipes</h1>
+      <Search onValueChange={searchCallback} />
       <ol className="flex flex-col">
-        {data.map((recipe: Recipe) => (
+        {recipes.map((recipe: Recipe) => (
           <li key={recipe.slug}>
             <Link
               className="underline hover:decoration-pink-500 hover:text-pink-500"
@@ -35,6 +55,7 @@ const Recipes = () => {
           </li>
         ))}
       </ol>
+      {recipes.length === 0 ? <SearchEmptyResults model="recipes" /> : null}
     </>
   );
 };
