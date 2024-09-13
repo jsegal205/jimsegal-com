76f27dec3158fd2791ddf466f67fa66a83be9100
Jim Segal <hello@jimsegal.com>
2024-09-13 12:49:00 -0500

    add game filter input

--- src/routes/_withnav/games/index.tsx
+++ src/routes/_withnav/games/index.tsx
@@ -1,14 +1,37 @@
-import { fetchAllGames } from "@/api/games";
-import { type Game } from "@/api/games";
-import { Loading } from "@/components/loading";
+import { useCallback, useEffect, useState } from "react";
 import { createFileRoute } from "@tanstack/react-router";
+
+import { fetchAllGames } from "@/api/games";
+import { type Game, type Games } from "@/api/games";
+import { Loading } from "@/components/loading";
 
 export const Route = createFileRoute("/_withnav/games/")({
   component: () => <Games />,
 });
 
 const Games = () => {
+  const [games, setGames] = useState<Games>([]);
+  const [search, setSearch] = useState<string>("");
   const { isError, isPending, data, error } = fetchAllGames();
+
+  useEffect(() => {
+    setGames(data || []);
+  }, [data]);
+
+  const searchCallback = useCallback(
+    (search: string) => {
+      if (search) {
+        const filteredGames = data!.filter((game) =>
+          game.name.toLowerCase().includes(search.toLowerCase()),
+        );
+
+        setGames(filteredGames);
+      } else {
+        setGames(data!);
+      }
+    },
+    [search, data],
+  );
 
   if (isPending) {
     return <Loading />;
@@ -22,8 +45,20 @@ const Games = () => {
     <>
       <h1>Games</h1>
       <p>My collection of {data.length} tabletop games.</p>
+      <div>
+        <label className="pr-2">Search:</label>
+        <input
+          className="border-2 border-slate-500 rounded p-1"
+          type="text"
+          value={search}
+          onChange={({ target: { value } }) => {
+            setSearch(value);
+            searchCallback(value);
+          }}
+        />
+      </div>
       <ol className="flex flex-wrap justify-evenly">
-        {data.map((game: Game) => (
+        {games.map((game: Game) => (
           <li key={game.bgg_id}>
             <a
               className="flex flex-col items-center border-2 border-slate-500 rounded m-5 p-4 underline w-[225px] hover:decoration-pink-500 hover:text-pink-500 hover:border-pink-500"
@@ -39,6 +74,13 @@ const Games = () => {
           </li>
         ))}
       </ol>
+
+      {games.length === 0 ? (
+        <div className="flex flex-col text-center">
+          <h2>Whoops, no games found</h2>
+          {Boolean(search) ? <div>Try updating the search</div> : null}
+        </div>
+      ) : null}
     </>
   );
 };
