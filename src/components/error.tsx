50a56b9f3c428f968d78da9a3437a47a33b30b42
Jim Segal <hello@jimsegal.com>
2024-09-18 15:58:26 -0500

    add button to check if your current browser location is colder than a… (#8)

--- src/components/error.tsx
+++ src/components/error.tsx
@@ -1,19 +1,24 @@
 import { Icon } from "@/icons";
 
-type Error = { message?: string };
+type Error = { message?: string; jimNotified?: boolean };
 
-export const Error = ({ message }: Error) => {
+export const Error = ({ message, jimNotified = true }: Error) => {
   return (
     <div className="flex flex-col">
       <div className="flex flex-row items-center">
         <Icon type="exclamation" className="w-6 fill-red-700 mr-2" />
         <label className="text-2xl font-bold text-red-700">Error</label>
       </div>
-      <label className="font-bold my-4">
-        Jim has been notified of this error. Please try again in a few minutes.
-      </label>
+      {jimNotified ? (
+        <>
+          <label className="font-bold my-4">
+            Jim has been notified of this error. Please try again in a few
+            minutes.
+          </label>
 
-      <label className="font-bold">Details:</label>
+          <label className="font-bold">Details:</label>
+        </>
+      ) : null}
       <label>{errorMessage(message)}</label>
     </div>
   );
