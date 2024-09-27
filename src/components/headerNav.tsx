4c3cdc7ec85b87e8afb636c7c58e791ad7472b69
Jim Segal <hello@jimsegal.com>
2024-09-27 10:59:18 -0500

    header in mono type

--- src/components/headerNav.tsx
+++ src/components/headerNav.tsx
@@ -3,7 +3,7 @@ import { Nav } from "@/components/nav";
 
 export const HeaderNav = () => (
   <header className="p-4 border-b-2 border-slate-400 flex justify-between">
-    <Link className="text-2xl !no-underline hover:!underline" to="/">
+    <Link className="font-mono text-2xl !no-underline hover:!underline" to="/">
       Jim Segal
     </Link>
 
