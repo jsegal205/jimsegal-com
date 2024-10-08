5073faf9be0f0835d9a3f971ba954d76074cf759
Jim Segal <hello@jimsegal.com>
2024-10-08 14:48:09 -0500

    update footer link to use common component

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -2,8 +2,10 @@ import React, { Suspense } from "react";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { Outlet, createRootRoute } from "@tanstack/react-router";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
-import { Icon } from "@/icons";
+
+import { Link } from "@/components/link";
 import { DarkModeProvider } from "@/contexts/darkMode";
+import { Icon } from "@/icons";
 
 const queryClient = new QueryClient();
 
@@ -16,6 +18,8 @@ const TanStackRouterDevtools = import.meta.env.PROD
       })),
     );
 
+// This is here to get around typescript errors relating to tanstack router type safety
+const githubSauce: string = "https://github.com/jsegal205/jimsegal-com";
 export const Route = createRootRoute({
   component: () => (
     <QueryClientProvider client={queryClient}>
@@ -23,24 +27,16 @@ export const Route = createRootRoute({
         <main className="bg-slate-100 dark:bg-slate-700 text-slate-900 dark:text-slate-100 min-h-screen flex flex-col justify-between">
           <Outlet />
 
-          <footer className="self-bottom">
+          <footer className="self-bottom mb-1">
             <div className="flex justify-center text-xs">
-              <span>Made with</span>
-              <Icon
-                className="mx-1 my-auto h-3 w-3 fill-red-500"
-                type="heart"
-              />
-              <span className="mr-1">by Jim Segal</span>(
-              <a
-                href="https://github.com/jsegal205/jimsegal-com"
-                className="mr-1 underline hover:decoration-pink-500 hover:text-pink-500 hover:fill-pink-500"
-              >
-                source
-              </a>
-              on{" "}
+              <span className="mr-1">Made with</span>
+              <Icon className="my-auto h-3 w-3 fill-red-500" type="heart" />
+              <span className="mx-1">by Jim Segal</span>(
+              <Link to={githubSauce}>source</Link>
+              <span className="mx-1">on</span>
               <Icon
                 type="github"
-                className="ml-1 my-auto h-3 w-3 dark:fill-slate-100"
+                className="my-auto h-3 w-3 dark:fill-slate-100"
               />
               )
             </div>
