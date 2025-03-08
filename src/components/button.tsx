dde017be3a0689f2ec89078967314b8484263b33
Jim Segal <jsegal205@gmail.com>
2025-03-08 13:54:16 -0600

    add button link prop and use for dark mode toggle

--- src/components/button.tsx
+++ src/components/button.tsx
@@ -1,15 +1,28 @@
+type ButtonKind = "button" | "link";
+
+type Props = React.ButtonHTMLAttributes<HTMLButtonElement> & {
+  kind?: ButtonKind;
+};
+
 export const Button = ({
   className,
   children,
+  kind = "button",
   ...rest
-}: React.ButtonHTMLAttributes<HTMLButtonElement>) => (
-  <button
-    className={[
-      "bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded disabled:bg-slate-400",
-      className,
-    ].join(" ")}
-    {...rest}
-  >
-    {children}
-  </button>
-);
+}: Props) => {
+  return (
+    <button className={[getClass(kind), className].join(" ")} {...rest}>
+      {children}
+    </button>
+  );
+};
+
+const getClass = (kind: ButtonKind) => {
+  switch (kind) {
+    case "link":
+      return "underline hover:decoration-pink-500 hover:text-pink-500 hover:fill-pink-500 dark:fill-slate-100 dark:hover:fill-pink-500";
+    default:
+      // case "button":
+      return "bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded disabled:bg-slate-400";
+  }
+};
